f3aed3dd6343682e55839a05a4ac7784c34390c9
Boning Chen <burning9967@outlook.com>
2024-12-25 09:46:56 +1100

    Clear repository content

--- server/backend/api/views/shop_view.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
-from ..models import Shop
-from ..serializer import ShopSerializer
-
-# Create your views here.
